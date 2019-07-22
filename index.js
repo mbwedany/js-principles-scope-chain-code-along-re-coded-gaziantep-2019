9125be8d7dea242c5e085c3267c9e20480ad2880
Mohamad Bwidani <mbwedany@gmail.com>
2019-07-22 21:59:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ function changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'someone worse'
 }
 
+var favoriteCustomer = 'me'
 
 function attemptTwoFavoriteCustomers(){
-  var favoriteCustomer = 'me'
-  var favoriteCustomer = 'one two three'
+  var favoriteCustomer = 'not me'
+  throw SyntaxError(SyntaxError)
 }
